7f07e2943fb952d13a4b0bcd7f5394fe173c436e
Sirisha Rambhatla <rambh002@umn.edu>
2015-08-11 15:40:57 -0500

    Implement normv() and doctests

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 ## normalize_easy Package
 
-This package implements normc() and normr() functions to easily normalize columns and rows respectively. 
-This package uses `sklearn.preprocessing.normalize` and forced the input array to have dtype as float. The input array has to be 2-D.
- 
-### Future Work
-* Implement doctests
+This package implements `normc()`, `normr()`, `normv()` functions to easily normalize columns, rows of 2-D arrays and vectors respectively. 
+This package uses `sklearn.preprocessing.normalize` and forced the input array to have dtype as float. The input array has to be 2-D for `normc()` and `normr()`. 
 
 
+* `normc()` -> normalizes columns of a 2-D array
+* `normr()` -> normalizes rows of a 2-D array
+* `normv()` -> normalizes the 1-D array
+
+
+### To install
+* `pip install normalize_easy`, or
+* Download the repo and run `python setup.py install`
 
 
 
--- normalize_easy/normalize_easy.py
+++ normalize_easy/normalize_easy.py
@@ -5,39 +5,73 @@ Sirisha Rambhatla
 rambh002@umn.edu
 """
 from numpy import asarray
+from pylab import rand, norm, allclose
 from sklearn.preprocessing import normalize
 
 # function called normc
 def normc(Mat):
-   """normalize the columns of the matrix
-   B= normc(A) normalizes the columns
-   the dtype of A is float"""
+   """Normalize the columns of the matrix
+   B= normc(A) 
+
+    >>> A = rand(4, 4)
+    >>> B = normc(A)
+    >>> allclose(norm(B[:, 0]), 1)
+    True"""
+   
+   # Assert 2-D Array
+   assert len(Mat.shape)==2 , 'The input array should be a 2-D array'
    
    # Enforce dtype float
-   if Mat.dtype!='float':
-      Mat = asarray(Mat,dtype=float)
-    
+   if Mat.dtype != 'float': Mat = asarray(Mat,dtype=float)
+   
+   # Normalize 
    B = normalize(Mat,norm='l2',axis=0)
    
    return B
    
    # function called normr
 def normr(Mat):
-   """normalize the columns of the matrix
-   B= normr(A) normalizes the row
-   the dtype of A is float"""
+   """Normalize the columns of the matrix
+   B= normr(A) 
+    
+    >>> A = rand(4, 4)
+    >>> B = normr(A)
+    >>> allclose(norm(B[0, :]), 1)
+    True
+    """
+   
+   # Assert 2-D Array
+   assert len(Mat.shape)==2 , 'The input array should be a 2-D array'
    
      # Enforce dtype float
-   if Mat.dtype!='float':
-      Mat = asarray(Mat,dtype=float)
-      
+   if Mat.dtype != 'float': Mat = asarray(Mat,dtype=float)
+       
    # if statement to enforce dtype float
    B = normalize(Mat,norm='l2',axis=1)
    
    return B
- 
+   
+def normv(Vec):
+    """Normalizes vectors
+    
+    >>> x = rand(5)
+    >>> y = normv(x)
+    >>> allclose(norm(y), 1)
+    True
+    """
+    
+    # Assert 2-D Array
+    assert len(Vec.shape)==1 , 'The input array should be a 1-D array'
+   
+    # Enforce dtype float
+    if Vec.dtype != 'float': Vec = asarray(Vec,dtype=float)
+      
+    # if statement to enforce dtype float
+    B = normalize(Vec,norm='l2')
+   
+    return B
+     
 # For doctests   
-#A=array([[1, 5, 0], [0, 2, 1], [1, 0, 2]])
-#B=normc(A)
-#norm(B[:,1])
-#norm(B[1,:])
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
